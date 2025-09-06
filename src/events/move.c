aa1d39861260f0accd1d003fd2f1f2c6f55a5f29
thiagoht10 <thiagodesouzaht@gmail.com>
2025-09-06 23:09:05 +0100

    so_long_v02

--- src/events/move.c
+++ src/events/move.c
@@ -6,11 +6,26 @@
 /*   By: thde-sou <thde-sou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 17:16:32 by thde-sou          #+#    #+#             */
-/*   Updated: 2025/09/06 21:30:08 by thde-sou         ###   ########.fr       */
+/*   Updated: 2025/09/06 23:06:14 by thde-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
+
+int	handle_key(int keycode, t_game *game)
+{
+	if (keycode == KEY_W)
+		move_up(game);
+	else if (keycode == 97)
+		move_left(game);
+	else if (keycode == 115)
+		move_down(game);
+	else if (keycode == 100)
+		move_right(game);
+	else if (keycode == KEY_ESC)
+		close_game(game);
+	return (0);
+}
 
 void	move_up(t_game *game)
 {
